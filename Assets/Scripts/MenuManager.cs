b14d91457eaaca9ad2f17cc94b2033c257857537
SyPhase <pi2568gamerz@gmail.com>
2025-03-08 20:46:50 -0700

    Finished Menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,6 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text highscoreCount;
+
+    const string _highscore = "highscore";
+
+    void Awake()
+    {
+        highscoreCount.text = PlayerPrefs.GetInt(_highscore, 0).ToString();
+    }
+
     void Update()
     {
         if (Input.anyKeyDown)
